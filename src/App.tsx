491a43133f7bbd0e063fc9677ad2b8540e9c0d4c
arsalan-tahir <arsalan.tahir@ceative.co.uk>
2022-11-22 05:25:09 -0800

    #comment

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,8 @@ function App() {
   return (
     <div className="">
       <Header />
-    
      <Containerbox />
+      <button className='btn btn-danger'>danger btn</button>
 
      <Footer />
       
